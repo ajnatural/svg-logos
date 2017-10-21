e3053cd330419fc11a054d7d506503347f5826d2
Anshul Jain <ajnatural@gmail.com>
2017-10-21 21:34:14 +1100

    Partially working

--- index.js
+++ index.js
@@ -6,33 +6,66 @@ const document = window.document
 const pd = require('pretty-data').pd
 
 const draw = SVG(document.documentElement)
-const url = 'https://raw.githubusercontent.com/iconic/open-iconic/master/svg/infinity.svg'
+const url = 'https://raw.githubusercontent.com/ajnatural/svg-logos/master/sample.svg';
 
 const  buildLogo = (params) => {
   fetch(params.icon).then(res => {
     return res.text()
   }).then(body => {
     const icon = buildIcon(body);
-    const heading = buildHeading(params.heading)
-    //const slogan = buildSlogan(params.slogan)
+    const heading = buildText(params.heading, params.heading_font, params.heading_size, params.heading_weight, params.heading_style)
+    const slogan = buildText(params.slogan, params.slogan_font, params.slogan_size, params.slogan_weight, params.slogan_style)
 
-    //heading.move(icon.width() + 10, 10)
-    //slogan.move(icon.width() + 10, heading.height() + 10)
+    switch (params.arrangement) {
+      case 'icon-left':
+        draw.attr('viewBox', '0 0 600 300')
+        draw.size(600, 300)
+
+        icon.size(null, 300)
+        heading.move(icon.width() + 10, 10)
+        slogan.move(icon.width() + 10, heading.attr('font-size') + 10)
+        break;
+      case 'icon-right':
+        draw.attr('viewBox', '0 0 600 300')
+        draw.size(600, 300)
+
+        icon.size(null, 300)
+        icon.move(600 - icon.width(), 0)
+        heading.move(600 - icon.width() - heading.bbox().width, 10)
+        slogan.move(600 - icon.width() - slogan.bbox().width, heading.attr('font-size') + 10)
+        break;
+      case 'icon-top':
+        draw.attr('viewBox', '0 0 600 600')
+        draw.size(600, 600)
+
+        icon.size(null, 300)
+        icon.center(300, 300)
+        heading.center(300, icon.height() + 10)
+        slogan.center(300, icon.height() + heading.attr('font-size') + 10)
+        break;
+    }
 
     saveLogo();
   });
 }
 
 const buildIcon = (icon) => {
-  return draw.svg(icon).scale(10, 10).move(0, 0);
+  icon = icon.replace('<svg', '<svg id="custom_logo" ')
+  draw.svg(icon)
+  const el = SVG.get('custom_logo')
+  el.attr('viewBox', null)
+  return el
 }
 
-const buildHeading = (heading) => {
-  return draw.text(heading);
-}
-
-const buildSlogan = (slogan) => {
-  return draw.text(slogan);
+const buildText = (text, font, size, weight, style) => {
+  const el = draw.plain(text)
+  el.font({
+    family: font,
+    size: size,
+    weight: weight,
+    style: style
+  })
+  return el
 }
 
 const saveLogo = () => {
@@ -47,5 +80,14 @@ const saveLogo = () => {
 buildLogo({
   icon: url,
   heading: 'Menith',
-  slogan: 'Design and Development'
+  slogan: 'Design and Development',
+  arrangement: 'icon-right',
+  heading_font: 'Helvetica',
+  heading_size: '32',
+  heading_style: 'italic',
+  heading_weight: '100',
+  slogan_font: 'Verdana',
+  slogan_size: '24',
+  slogan_style: 'oblique',
+  slogan_weight: '300'
 });
